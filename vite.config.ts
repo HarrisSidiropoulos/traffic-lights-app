f2db562d4c508a481bfeb80dad4e56310276f0c4
Harris Sidiropoulos <theocharis.sidiropou@gmail.com>
2025-09-21 14:26:58 +0200

    ci: Update build command for GitHub Pages deployment and configure base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+// Set base path conditionally for GitHub Pages
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+const base = isGithubPages ? "/traffic-lights-app/" : "/";
+
 export default defineConfig({
+  base,
   plugins: [react()],
-})
+});
