ed74dec1cd92fee120541097bee7692aa009bf89
Harris Sidiropoulos <theocharis.sidiropou@gmail.com>
2025-09-21 14:33:35 +0200

    fix: Remove redundant build step and ensure correct environment variable usage for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 // Set base path conditionally for GitHub Pages
-
 const isGithubPages = process.env.GITHUB_PAGES === "true";
 const base = isGithubPages ? "/traffic-lights-app/" : "/";
-console.log(
-  `Vite config: GITHUB_PAGES='${process.env.GITHUB_PAGES}', base='${base}'`
-);
 
 export default defineConfig({
   base,
